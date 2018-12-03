ac6f58c09eeaf58630dc3b9a27e81421046076b4
MaxEC <maximiliano.edwards@ing.uchile.cl>
2018-12-03 16:01:30 -0300

    Naive listo

--- src/NaiveDijkstra.java
+++ src/NaiveDijkstra.java
@@ -1,10 +1,9 @@
-/**
- * Created by GPG on 11/30/2018.
- */
+import java.util.List;
+
 public class NaiveDijkstra {
 
     private float[] dist;
-    private boolean[] marcado;
+    private boolean[] mark;
     private Node[] prev;
 
     public NaiveDijkstra(int n) {
@@ -13,7 +12,7 @@ public class NaiveDijkstra {
             dist[i] = Float.MAX_VALUE;
         }
         // default initialization is false
-        marcado = new boolean[n];
+        mark = new boolean[n];
         prev = new Node[n];
     }
 
@@ -21,8 +20,8 @@ public class NaiveDijkstra {
         return dist;
     }
 
-    public boolean[] getMarcado() {
-        return marcado;
+    public boolean[] getMark() {
+        return mark;
     }
 
     public Node[] getPrev() {
@@ -31,21 +30,62 @@ public class NaiveDijkstra {
 
     public void run(GraphOfNodes g) {
         int V = g.getNumVertex();
-        for (int i = 1; i < V; i++) {
+        for (int i = 0; i < V; i++) {
             float minDist = Float.MAX_VALUE;
             // Node value
             int minNode = -1;
 
             // Find minimum
-            for (int j = 1; j < V; j++) {
-                if (!marcado[j] && dist[j] < minDist) {
+            for (int j = 0; j < V; j++) {
+                if (!mark[j] && dist[j] < minDist) {
                     minDist = dist[j];
                     minNode = j;
                 }
             }
-
             int u = minNode;
+            mark[u] = true;
+            Node nu = g.getNodes().get(u);
+            for (Edge edge : nu.getConnections()) {
+                int v = edge.getEnd().getValue();
+                if (dist[v] > dist[u] + edge.getWeight()) {
+                    dist[v] = dist[u] + edge.getWeight();
+                    prev[v] = nu;
+                }
+            }
         }
+        prev[0] = g.getNodes().get(0);
+    }
+
+    public static void main(String[] args) {
+        int n = 6;   //1000;
+        int e = 10;   //100*n;
+        GraphOfNodes g = new GraphOfNodes(n, e);
+        g.populate();
+        NaiveDijkstra nd = new NaiveDijkstra(n);
+        nd.run(g);
+        // /*
+        // Print Graph
+        List<Node> nodeList = g.getNodes();
+        for (Node node : nodeList) {
+            System.out.println("Node: " + node.getValue());
+            System.out.println("Edges: ");
+            for (Edge edge : node.getConnections()) {
+                System.out.println(" To node: " + edge.getEnd().getValue() + " peso: " + edge.getWeight());
+            }
+            System.out.println("-----------");
+        }
+        System.out.println("\nPrinting optimum connections\n");
+        // Print optimum connections
+        Node[] prev = nd.getPrev();
+        for (int i = 1; i < n; i++) {
+            Node prevNode = prev[i];
+            Node thisNode = g.getNodes().get(i);
+            float[] allDists = nd.getDist();
+            float w = prevNode.getConnectionWeight(thisNode);
+            System.out.println("Previous node " + prevNode.getValue() + " to this node " + thisNode.getValue());
+            System.out.println(" Weight from zero to this node: " + allDists[i]);
+        }
+        // */
     }
 
 }
--- src/Node.java
+++ src/Node.java
@@ -19,8 +19,24 @@ public class Node {
         return connections;
     }
 
+    public float getConnectionWeight(Node node) {
+        for (Edge edge : this.connections) {
+            if (edge.getEnd().equals(node)) {
+                return edge.getWeight();
+            }
+        }
+        return 0;
+    }
+
     public void addEdge(Edge e) {
         this.connections.add(e);
+    }
+
+    public void printEdges() {
+        System.out.println("Edges: ");
+        for (Edge edge : this.connections) {
+            System.out.println(" To node: " + edge.getEnd().getValue() + " peso: " + edge.getWeight());
+        }
     }
 
 }
