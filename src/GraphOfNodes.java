7aa3ef880c3feea104ab48230a2a25762b106826
MaxEC <maximiliano.edwards@ing.uchile.cl>
2018-12-03 15:01:07 -0300

    Grafo usando buenas practicas de programacion

--- src/GraphOfNodes.java
+++ src/GraphOfNodes.java
@@ -5,25 +5,37 @@ import java.util.Random;
 public class GraphOfNodes {
 
     private int numVertex;
+    private int numEdges;
     private List<Node> nodes;
 
     public GraphOfNodes(int n, int e) {
-        numVertex = n;
+        this.numVertex = n;
+        this.numEdges = e;
         nodes = new ArrayList<>();
+    }
 
+    public int getNumVertex() {
+        return numVertex;
+    }
+
+    public List<Node> getNodes() {
+        return nodes;
+    }
+
+    public void populate() {
         // Random float between 0 inclusive and 1 exclusive
         Random rand = new Random();
         // Random int between 0 and n
         Random rand2 = new Random();
 
         // Generate nodes
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < this.numVertex; i++) {
             Node node = new Node(i);
             nodes.add(node);
         }
 
         // Generate edges to ensure connectivity
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < this.numVertex - 1; i++) {
             float r = rand.nextFloat();
             // Change random number 0 to 1 -> now 0 exclusive and 1 inclusive
             float new_r = (r == 0) ? 1 : r;
@@ -36,15 +48,15 @@ public class GraphOfNodes {
         }
 
         // Generate rest e - n + 1 edges
-        int rest = e - n + 1;
+        int rest = this.numEdges - this.numVertex + 1;
         // Number of places edges
-        int count = n - 1;
-        for (int i = rest; i < e; i++) {
+        int count = this.numVertex - 1;
+        for (int i = rest; i < this.numEdges; i++) {
             // Possible new edge
             boolean notReady = true;
             while (notReady) {
-                int v1 = rand2.nextInt(n);
-                int v2 = rand2.nextInt(n);
+                int v1 = rand2.nextInt(this.numVertex);
+                int v2 = rand2.nextInt(this.numVertex);
                 Node n1 = nodes.get(v1);
                 Node n2 = nodes.get(v2);
                 // Don't include edge from and to the same vertex
@@ -67,7 +79,7 @@ public class GraphOfNodes {
                         Edge edge2 = new Edge(n2, n1, new_r);
                         n1.addEdge(edge);
                         n2.addEdge(edge2);
-                        count++; // ?
+                        count++;
                         notReady = false;
                     }
                 }
@@ -75,19 +87,12 @@ public class GraphOfNodes {
         }
     }
 
-    public int getNumVertex() {
-        return numVertex;
-    }
-
-    public List<Node> getNodes() {
-        return nodes;
-    }
-
     public static void main(String[] args) {
         double iniTime = System.currentTimeMillis();
         int n = 1000000;
         int e = 1000*n;
         GraphOfNodes g = new GraphOfNodes(n, e);
+        g.populate();
         //List<Node> nodeList = g.getNodes();
         /*
         for (Node node : nodeList) {
